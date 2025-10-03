0e23aac7e93a8b5bf98c480896f789a96d493f8c
DillonIs <dillonis826@gmail.com>
2025-10-03 11:39:05 +1000

    Basic thoughts for Recipe.py

---
+++ Chaos_cooking_backend/recipes.py
@@ -0,0 +1,32 @@
+# Recipe Back-end Logic
+"""Import everything that is needed from other files"""
+
+
+
+# Define the many-to-many table
+
+"""Recipes should have to the User-id that created it"""
+
+# Create a Recipe class
+"""
+    Recipe class:
+    Recipe Title
+    Recipe id - Unique ID for recipes
+    Created_by - User-ID that added the recipe
+    Created_at
+    Updated_at
+    Description? - Brief explanation of Recipe
+    Ingredients - Ingredients needed
+    Method - How to make the recipe
+    Time - How long it takes to make
+    Difficulty? - How easy or challenging the recipe is
+    Reviews? - A few reviews if the recipe is public?
+"""
+
+class Recipe():
+
+    def __init__(self):
+
+# -- Getters and Setters --
+
+# --- Methods ---
