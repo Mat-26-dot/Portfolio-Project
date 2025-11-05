b4a8ee6931ac0c70ce2c13e56c7fa37c358451b4
DillonIs <dillonis826@gmail.com>
2025-11-06 09:45:50 +1100

    Debugging virtual pet

--- Chaos_cooking_backend/app.py
+++ Chaos_cooking_backend/app.py
@@ -15,7 +15,7 @@ from routes.virtualpet import virtual_pet_bp
 # Register blueprints
 app.register_blueprint(recipes_bp, url_prefix='/api/recipes')
 app.register_blueprint(ingredients_bp, url_prefix='/api/ingredients')
-app.register_blueprint(virtual_pet_bp, url_prefix='/virtual-pet')
+app.register_blueprint(virtual_pet_bp, url_prefix='/api/virtual-pet')
 
 @app.route('/')
 def home():
